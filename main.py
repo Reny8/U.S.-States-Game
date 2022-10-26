77bc64077dba450b1699b6f2c1f2c909eb8cd63c
Reny Reyes <renyreyes8@hotmail.com>
2022-10-26 17:38:26 -0400

    Adjusted coordinates

--- main.py
+++ main.py
@@ -13,13 +13,14 @@ turtle.shape(background)
 screen.bgcolor("black")
 
 #FUNCTIONALITY
-guess = screen.textinput(title = "Guess a State", prompt = "What is one of the state's name?").capitalize()
-found = data[data.state == guess]
+while True:
+    guess = screen.textinput(title = "Guess a State", prompt = "What is one of the state's name?").capitalize()
+    found = data[data.state == guess]
 
-if not found.empty:
-    pointer = turtle.Turtle()
-    pointer.hideturtle()
-    pointer.penup()
-    pointer.goto(int(found.x),int(found.y))
-    pointer.write(guess)
+    if not found.empty:
+        pointer = turtle.Turtle()
+        pointer.hideturtle()
+        pointer.penup()
+        pointer.goto(int(found.x),int(found.y))
+        pointer.write(guess)
 screen.exitonclick()
