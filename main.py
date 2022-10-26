737ec65340ede2ae3e08eb3bf08e241d4de9eac1
Reny Reyes <renyreyes8@hotmail.com>
2022-10-26 17:45:25 -0400

    Added the limit of 50 states

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import turtle
-from numpy import empty
 import pandas
 #VARIABLES
 screen = turtle.Screen()
 background = "blank_states_img.gif"
 data = pandas.read_csv("50_states.csv")
+guessed_states = []
 #SCREEN PROPERTIES
 screen.title("U.S. States Game")
 screen.addshape(background)
@@ -13,14 +13,17 @@ turtle.shape(background)
 screen.bgcolor("black")
 
 #FUNCTIONALITY
-while True:
-    guess = screen.textinput(title = "Guess a State", prompt = "What is one of the state's name?").capitalize()
+while len(guessed_states) < 50:
+    guess = screen.textinput(title = f"{len(guessed_states)} / 50 States Correct", prompt = "What is one of the state's name?").capitalize()
     found = data[data.state == guess]
 
     if not found.empty:
+        guessed_states.append(guess)
         pointer = turtle.Turtle()
         pointer.hideturtle()
         pointer.penup()
         pointer.goto(int(found.x),int(found.y))
         pointer.write(guess)
-screen.exitonclick()
+
+
+# screen.exitonclick()
